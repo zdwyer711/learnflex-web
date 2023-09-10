069e00bb88ff499451fd52a373f048dff4c2678c
Zach Dwyer <55959541+zdwyer711@users.noreply.github.com>
2023-09-10 14:21:45 -0500

    Remove useEffect and useState methods from footer components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { FooterWrapper, FooterText, FooterGridWrapper } from './Footer.styled';
 
 function Footer() {
-  const mediaMatch = window.matchMedia('(min-width: 1200px)');
-  const [matches, setMatches] = useState(mediaMatch.matches);
-  
-  useEffect(() => {
-    const handler = e => setMatches(e.matches);
-    mediaMatch.addListener(handler);
-    return () => mediaMatch.removeListener(handler);
-  });
 
   return(
     <FooterWrapper>
